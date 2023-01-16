22ab0673e4c05aff40c085e7224ac1e2adcecef3
Eugene <eugeneayonga@gmail.com>
2023-01-16 16:19:18 +0300

    First Commit

---
+++ bin/dart_built_in_types.dart
@@ -0,0 +1,5 @@
+import 'package:dart_built_in_types/dart_built_in_types.dart' as dart_built_in_types;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_built_in_types.calculate()}!');
+}
---
+++ test/dart_built_in_types_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_built_in_types/dart_built_in_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
